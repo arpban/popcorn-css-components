e6fee7b58c8da79a9fc4e39db57e881b1f553e07
ArpitX <arpit.bansal2010@gmail.com>
2017-08-16 09:47:48 +0530

    new components

---
+++ dropdown-menus/slidein/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+//Dependencies : jQuery
+function openDrawer() {
+        $('.drawer-handle').toggleClass('open');
+        $('.drawer-space').toggleClass('open');
+}
---
+++ modals/basic/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+//Dependencies : jQuery
+function activateModal() {
+    $('.modal').toggleClass('open');
+}
